19365c6b86b12ee66ee04ce6509174bd7610bd2e
XurshidovichBekzod <xurshidovichbekzod@gmail.com>
2025-09-20 00:08:17 +0500

    +

--- src/features/profile/service/useProfile.tsx
+++ src/features/profile/service/useProfile.tsx
@@ -1,27 +1,23 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { api } from "../../../shared/api";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { api } from "../../../shared/api"
 
-export const profileKey = "profileKey";
+export const profileKey = "profileKey"
 
 export const useProfile = () => {
-  const queryClient = useQueryClient();
 
-  const getUser = () =>
-    useQuery<any, any>({
-      queryKey: [profileKey],
-      queryFn: () => api.get("/user").then((res) => res.data), // interceptor tokenni qo'shib yuboradi
+    const queryClient = useQueryClient();
+
+
+    const getUser = () => useQuery<any, any>({
+        queryKey: [profileKey],
+        queryFn: () => api.get("user").then(res => res.data)
+    })
+
+    const updateProfile = useMutation<any, any, { id: string; body: any }>({
+        mutationFn: ({ id, ...body }) => api.patch(`category/${id}`, body).then((res) => res.data),
+        onSuccess: () => queryClient.invalidateQueries({ queryKey: [profileKey] })
     });
 
-  const updateProfile = useMutation<
-    any,
-    any,
-    { id: string; body: any }
-  >({
-    mutationFn: ({ id, body }) =>
-      api.patch(`/user/${id}`, body).then((res) => res.data), // category emas user update
-    onSuccess: () =>
-      queryClient.invalidateQueries({ queryKey: [profileKey] }),
-  });
 
-  return { getUser, updateProfile };
-};
+    return { getUser, updateProfile }
+}
